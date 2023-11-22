3c7c9c2cc56385280a96a949b068c0a02a013c8a
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-11-22 13:12:28 -0500

    firebase configured and initialized

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,5 @@
 // authRoutes 
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.get('/logout', authController.logout);
-router.get('/status', authController.checkAuthStatus);
 
 module.exports = router;
